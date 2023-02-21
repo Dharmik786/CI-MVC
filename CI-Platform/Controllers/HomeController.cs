289b67345c40a5e37d9e2fd86a54d63fe8a54e7e
Dharmik786 <dkpatel96011@gmail.com>
2023-02-21 16:12:37 +0530

    ci_mvc

---
+++ CI-Platform/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using CI_Platform.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CI_Platform.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Registration()
+        {
+            return View();
+        }
+        public IActionResult Lost_Password()
+        {
+            return View();
+        }
+        public IActionResult Reset_Password()
+        {
+            return View();
+        }
+        public IActionResult landingpage()
+        {
+            return View();
+        }
+        public IActionResult NoMissionFound()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
