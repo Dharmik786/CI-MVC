a97e84b5c0a07b27ce19442d4361ebb7ad64252c
Dharmik786 <dkpatel96011@gmail.com>
2023-04-04 12:49:33 +0530

    ci

---
+++ CI-Platform/wwwroot/js/TimeSheet.js
@@ -0,0 +1,42 @@
+﻿function Delete(id) {
+    debugger;
+    $.ajax({
+        type: "POST",
+        url: "/Home/DeleteTimeSheet",
+        data: { 'id': id },
+        success: function (result) {
+            $('.tbl').html($(result).find('.tbl').html());
+        },
+        error: function () {
+            console.log("Erroor");
+        }
+    });
+}
+
+function EditTimeSheet(id) {
+    $.ajax({
+        type: "POST",
+        url: "/Home/EditTimeSheet",
+        data: { id: id },
+        success: function (result) {
+
+            var mission = document.getElementById('mission');
+            mission.value = result.timesheet.missionId;
+
+            var action = document.getElementById('Gaction');
+            action.value = result.timesheet.action; 
+
+            var dateVolunteered = document.getElementById('dateVolunteered');
+            dateVolunteered.value = result.timesheet.dateVolunteered;
+
+            var notes = document.getElementById('notes');
+            notes.value = result.timesheet.notes;
+
+        },
+        error: function () {
+            console.log("Erroor");
+        }
+    });
+}
+
+
